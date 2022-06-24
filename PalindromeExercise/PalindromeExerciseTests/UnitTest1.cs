7256421c8ab339a8fe9e685db5a5b56b80367636
David Taccetta <taccetta14@gmail.com>
2022-06-24 13:00:50 -0400

    Initial PalindromeExercise Commit

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
-namespace PalindromeExerciseTests
+namespace PalindromeExercise.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("David", false)]
+        [InlineData("madam", true)]
+        [InlineData("hotdog", false)]
+        [InlineData("civic", true)]
+        public void IsAPalindrome_Given_Word1_Should_ReturnBool(string word1, bool expected)
         {
+            WordSmith challenger = new WordSmith();
 
+            var actual = challenger.IsAPalindrome(word1);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
